7d533251a1f0eb18e195c25da7ee3f48bca3da54
tomvsaji <tomvellavoor@gmail.com>
2021-11-02 09:15:58 +0530

    backend done and front end started

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ connection.once('open', () =>{
 })
 
 const exercisesRouter = require('./routes/exercises');
-const usersRouter = require('./routes/user');
+const usersRouter = require('./routes/users');
 
 
 app.use('/exercises', exercisesRouter);
